1d037ed98149578d2b71148bde50545af9eb0b1f
tatto2k3 <ngotattopq@gmail.com>
2024-05-10 16:07:10 +0700

    first commit

--- src/main/java/com/example/boatbookingbe/repository/BoatRepository.java
+++ src/main/java/com/example/boatbookingbe/repository/BoatRepository.java
@@ -3,9 +3,9 @@ package com.example.boatbookingbe.repository;
 
 import com.example.boatbookingbe.entity.Boat;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
+
 public interface BoatRepository extends JpaRepository<Boat, Long> {
 
 }
--- src/main/java/com/example/boatbookingbe/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/boatbookingbe/service/impl/UserServiceImpl.java
@@ -2,16 +2,18 @@ package com.example.boatbookingbe.service.impl;
 
 import com.example.boatbookingbe.repository.UserRepository;
 import com.example.boatbookingbe.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetailsService userDetailsService(){
